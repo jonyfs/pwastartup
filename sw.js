832d6ecbf8e2ece09c93245afc663f66fad6b7ff
Jony Santos <jony.santos@bematech.com.br>
2018-05-08 18:37:52 -0300

    initial commit

--- sw.js
+++ sw.js
@@ -1,22 +1,13 @@
-
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
   console.log('[Service Worker] Activating Service Worker ...', event);
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   console.log('[Service Worker] Fetching something ....', event);
   event.respondWith(fetch(event.request));
-});
-
-self.addEventListener('push', function(event) {
-  if (event.data) {
-    console.log('This push event has data: ', event.data.text());
-  } else {
-    console.log('This push event has no data.');
-  }
 });
