004a3d61142a9967d7ef2dd1368354ff351d2d8c
Jony Santos <jony.santos@bematech.com.br>
2018-05-15 18:38:25 -0300

    initial commit

--- src-sw.js
+++ src-sw.js
@@ -3,3 +3,16 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 console.log('service worker config');
 
 workbox.precaching.precacheAndRoute([]);
+
+workbox.routing.registerRoute(
+    /(.*)articles(.*)\.(?:png|gif|jpg)/,
+    workbox.strategies.cacheFirst({
+      cacheName: 'images-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        })
+      ]
+    })
+  );
--- sw.js
+++ sw.js
@@ -156,3 +156,16 @@ workbox.precaching.precacheAndRoute([
     "revision": "8f5c0733c79056ceb5a5189b13f7ec86"
   }
 ]);
+
+workbox.routing.registerRoute(
+    /(.*)articles(.*)\.(?:png|gif|jpg)/,
+    workbox.strategies.cacheFirst({
+      cacheName: 'images-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        })
+      ]
+    })
+  );
