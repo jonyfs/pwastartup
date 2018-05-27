77706ad37a1dcc934b18d2ce1ff1fa8cc67096d7
Jony Santos <jonyferreira@hotmail.com>
2018-05-26 22:53:48 -0300

    initial commit

--- assets/js/readVersion.js
+++ assets/js/readVersion.js
@@ -1,5 +1,7 @@
-$.getJSON("version.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
+$().ready(function() {
+    $.getJSON("version.json", function(json) {
+        console.log(json); // this will show the info it in firebug console
 
-    document.getElementById('build-date').innerHTML = 'Build Date:' + json.buildDate;
+        document.getElementById('build-date').innerHTML = 'Build Date:' + json.buildDate;
+    });
 });
